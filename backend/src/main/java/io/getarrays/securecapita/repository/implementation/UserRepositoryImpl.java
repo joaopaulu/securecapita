d680322814148310f4b0fcddbf82a7905f233feb
joaopaulu <jptick@gmail.com>
2023-06-23 20:29:59 -0300

    Start to Implement UserRepository

---
+++ backend/src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -0,0 +1,43 @@
+package io.getarrays.securecapita.repository.implementation;
+
+import io.getarrays.securecapita.domain.model.User;
+import io.getarrays.securecapita.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class UserRepositoryImpl implements UserRepository<User> {
+
+    private final NamedParameterJdbcTemplate jdbc;
+
+    @Override
+    public User create(User data) {
+        return null;
+    }
+
+    @Override
+    public Collection<User> list(int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public User get(Long id) {
+        return null;
+    }
+
+    @Override
+    public User update(User data) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
