77d8309d99c2fc429e11df923e70db25637b24bd
joaopaulu <jptick@gmail.com>
2023-06-23 21:33:59 -0300

    Implementing UserRepository

--- backend/src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
+++ backend/src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package io.getarrays.securecapita.repository.implementation;
 
 import io.getarrays.securecapita.domain.model.User;
+import io.getarrays.securecapita.exception.ApiException;
 import io.getarrays.securecapita.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,16 +9,21 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
 @Slf4j
 public class UserRepositoryImpl implements UserRepository<User> {
 
+    private static final String COUNT_USER_EMAIL_QUERY = "";
     private final NamedParameterJdbcTemplate jdbc;
 
     @Override
-    public User create(User data) {
+    public User create(User user) {
+        if(getEmailCount(user.getEmail().trim().toLowerCase()) > 0){
+            throw new ApiException("Email already in use. Please use a different email and try again");
+        }
         return null;
     }
 
@@ -39,5 +45,9 @@ public class UserRepositoryImpl implements UserRepository<User> {
     @Override
     public Boolean delete(Long id) {
         return null;
+    }
+
+    private Integer getEmailCount(String email){
+        return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
     }
 }
