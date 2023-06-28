331bfd5aae679731023be8defa20b3dd25c8519f
joaopaulu <jptick@gmail.com>
2023-06-28 18:58:38 -0300

    Updating UserRepositoryImpl

--- backend/src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
+++ backend/src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -8,19 +8,24 @@ import io.getarrays.securecapita.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 import static io.getarrays.securecapita.domain.enumeration.RoleType.ROLE_USER;
-import static io.getarrays.securecapita.query.UserQuery.COUNT_USER_EMAIL_QUERY;
-import static io.getarrays.securecapita.query.UserQuery.INSERT_USER_QUERY;
+import static io.getarrays.securecapita.domain.enumeration.VerificationType.ACCOUNT;
+import static io.getarrays.securecapita.query.UserQuery.*;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -29,10 +34,11 @@ public class UserRepositoryImpl implements UserRepository<User> {
 
     private final NamedParameterJdbcTemplate jdbc;
     private final RoleRepository<Role> roleRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public User create(User user) {
-        if(getEmailCount(user.getEmail().trim().toLowerCase()) > 0){
+        if (getEmailCount(user.getEmail().trim().toLowerCase()) > 0) {
             throw new ApiException("Email already in use. Please use a different email and try again");
         }
         try {
@@ -41,10 +47,18 @@ public class UserRepositoryImpl implements UserRepository<User> {
             jdbc.update(INSERT_USER_QUERY, parameterSource, holder);
             user.setId(Objects.requireNonNull(holder.getKey()).longValue());
             roleRepository.addRoleToUser(user.getId(), ROLE_USER.name());
-        }catch (EmptyResultDataAccessException exception){
-
+            String verificationUrl = getVerificationUrl(UUID.randomUUID().toString(), ACCOUNT.getType());
+            jdbc.update(INSERT_ACCOUNT_VERIFICATION_URL_QUERY, Map.of("userId", user.getId(), "url", verificationUrl));
+            //emailService.sendVerificationUrl(user.getFirstName(), user.getEmail(), verificationUrl, ACCOUNT.getType());
+            user.setEnabled(false);
+            user.setNotLocked(true);
+            return user;
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ApiException("No role found by name: " + ROLE_USER.name());
+        } catch (Exception exception) {
+            throw new ApiException("An error occurred. Please try again.");
         }
-        return null;
+
     }
 
     @Override
@@ -67,11 +81,19 @@ public class UserRepositoryImpl implements UserRepository<User> {
         return null;
     }
 
-    private Integer getEmailCount(String email){
+    private Integer getEmailCount(String email) {
         return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
     }
 
-    private SqlParameterSource getSqlParameterSource(User user){
-        return null;
+    private SqlParameterSource getSqlParameterSource(User user) {
+        return new MapSqlParameterSource()
+                .addValue("firstName", user.getFirstName())
+                .addValue("lastName", user.getLastName())
+                .addValue("email", user.getEmail())
+                .addValue("password", encoder.encode(user.getPassword()));
+    }
+
+    private String getVerificationUrl(String key, String type) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/verify" + type + key).toUriString();
     }
 }
